275be0262c201d6ae70f5c76c27c64ef08ded310
Justin Celmer <99987430+c3lm3r@users.noreply.github.com>
2025-05-21 12:46:08 -0400

    Update app.py
    Include redisDB tracking visit count externally.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+redisDB = Redis(host="redis-db", port=6380)
 
 @app.route('/')
 def welcomeToKodeKloud();
+  redisDB.incr('visitorCount')
+  visitCount = str(redisDB.get('visitorCount', 'utf-8')
   return "Welcome to KODEKLOUD!"
 
 if __name__ == "__main__":
